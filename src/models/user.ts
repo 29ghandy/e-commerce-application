8fb21ff0b4ef805a3d6ef03d4bab6220d8ffb6c5
Omar mamdouh <omarmamdouh753@gmail.com>
2025-04-10 22:17:22 +0200

    Revert "user model done"
    This reverts commit c5e26f64e0724bee70bf216a44d11868bad2d8a8.

--- src/models/user.ts
+++ src/models/user.ts
@@ -14,15 +14,6 @@ const User = sequelize.define('user', {
     name: {
         type: DataTypes.STRING,
         allowNull: false
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-    ,
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
     }
 });
 
