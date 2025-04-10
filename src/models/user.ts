360cbb7b59dc39dffea62439ebaf4c4b9e1369b2
Omar mamdouh <omarmamdouh753@gmail.com>
2025-04-10 22:22:13 +0200

    Reapply "user model done"
    This reverts commit 8fb21ff0b4ef805a3d6ef03d4bab6220d8ffb6c5.

--- src/models/user.ts
+++ src/models/user.ts
@@ -14,6 +14,15 @@ const User = sequelize.define('user', {
     name: {
         type: DataTypes.STRING,
         allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+    ,
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
     }
 });
 
